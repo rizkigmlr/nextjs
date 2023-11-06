ae114a4afc57fa4bb8a0a88e15c3ae4238f38cf8
rizkigmlr <146905991+rizkigmlr@users.noreply.github.com>
2023-11-06 19:02:46 +0700

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Navbar from './component/NavBar';
 
-const inter = Inter({ subsets: ['latin'] })
+const Home = () => (
+    <div>
+        <Navbar />
+        <h1>Home Page</h1>
+    </div>
+);
 
-export default function Home() {
-  return (
-    <>
-      <h1 className={styles.center}>Rizky Gumilar</h1>
-    </>
-  )
-}
+export default Home;
